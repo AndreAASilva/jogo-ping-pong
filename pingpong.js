2020cf4a3a559d033680cef739f9acb31c9c1349
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-11-22 14:40:58 -0300

    Finalizado gráficos
    Finalizada parte gráfica e iniciado código javascript

---
+++ pingpong.js
@@ -0,0 +1,44 @@
+//Elemntos do jogo
+
+var vbtIniciar;
+var vbola;
+var vcpu;
+var vjogador;
+var vPaineltxtPontos;
+
+//Controles de animação
+var game, frames;
+
+//Posições dos elementos 
+
+var posBolaX, posBolaY;
+var posJogadorX, posJogadorY;
+var posCpuX, posCpuY;
+
+//Direção de acordo com o teclado
+
+var dirJy;
+
+//Posições iniciais
+
+var posJogIniJ=180, posCpuIniY=180, posBolaIniX=475,posBolaIniY=240;
+
+//Tamanhos
+
+var campoX=0, campoY=0, campoW=960, campoH=500;
+var barraW=20, barraH=140, bolaW=20, bolaH=20;
+
+
+//Direção
+
+var bolaX, bolaY;
+var jogY=0, CpuY=0;
+
+//Velocidade
+
+var velBola, velCpu, velJogador;
+
+//Controle
+
+var pontos=0;
+var tecla;
