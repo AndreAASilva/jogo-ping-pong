72ae987a9a0014131c0e60d4a03e51833be35555
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-11-23 04:24:22 -0300

    Finalizado segunda parte
    Inserida as primeiras funções do jogo

--- pingpong.js
+++ pingpong.js
@@ -1,44 +1,109 @@
-//Elemntos do jogo
+ //Elemntos do jogo
 
-var vbtIniciar;
-var vbola;
-var vcpu;
-var vjogador;
-var vPaineltxtPontos;
+ var vbtIniciar;
+ var vbola;
+ var vcpu;
+ var vjogador;
+ var vPaineltxtPontos;
 
-//Controles de animação
-var game, frames;
+ //Controles de animação
 
-//Posições dos elementos 
+ var game, frames;
 
-var posBolaX, posBolaY;
-var posJogadorX, posJogadorY;
-var posCpuX, posCpuY;
+ //Posições dos elementos 
 
-//Direção de acordo com o teclado
+ var posBolaX, posBolaY;
+ var posJogadorX, posJogadorY;
+ var posCpuX, posCpuY;
 
-var dirJy;
+ //Direção de acordo com o teclado
 
-//Posições iniciais
+ var dirJy;
 
-var posJogIniJ=180, posCpuIniY=180, posBolaIniX=475,posBolaIniY=240;
+ //Posições iniciais
 
-//Tamanhos
+ var posJogIniJ=180, posCpuIniY=180, posBolaIniX=475,posBolaIniY=240;
 
-var campoX=0, campoY=0, campoW=960, campoH=500;
-var barraW=20, barraH=140, bolaW=20, bolaH=20;
+ //Tamanhos
+
+ var campoX=0, campoY=0, campoW=960, campoH=500;
+ var barraW=20, barraH=140, bolaW=20, bolaH=20;
 
 
-//Direção
+ //Direção
 
-var bolaX, bolaY;
-var jogY=0, CpuY=0;
+ var bolaX, bolaY;
+ var jogY=0, CpuY=0;
 
-//Velocidade
+ //Velocidade
 
-var velBola, velCpu, velJogador;
+ var velBola, velCpu, velJogador;
 
-//Controle
+ //Controle
 
-var pontos=0;
-var tecla;
+ var pontos=0;
+ var tecla;
+ jogo=false; //Verificar se o jogo está rolando ou não
+
+ function controlajog(){
+     if(jogo){
+         posJogadorY+=velJogador*dirJy;
+         vjogador.style.top=posJogadorY+'px';
+     }
+ }
+
+
+ function teclaDw(){
+     tecla=event.keyCode;
+         if(tecla==38){//CIMA
+             dirJy=-1;
+         }else if(tecla==40){//BAIXO
+             dirJy=+1;
+         }
+ }
+
+ function teclaUp(){
+     tecla=event.keyCode;
+         if(tecla==38){//CIMA
+             dirJy=0;
+         }else if(tecla==40){//BAIXO
+             dirJy=0;
+         }
+ }
+
+
+ function game(){
+     if(jogo){
+         controlajog()
+     }
+     frames=requestAnimationFrame(game)
+ }
+
+
+ function iniciaJogo(){
+     if(!jogo){
+         cancelAnimationFrame(frames)
+         jogo=true;
+         dirJy=0;
+         posBolaX=posBolaIniX;
+         posBolaY=posBolaIniY;
+         posJogadorY=0;
+         posCpuY=posCpuIniY;
+         game()
+     }
+ }
+
+
+ function inicializa(){
+     velBola=velCpu=velJogador=8;
+     vbtIniciar=document.getElementById('btIniciar')
+     vbtIniciar.addEventListener('click',iniciaJogo)
+     vjogador=document.getElementById('dvJogador')
+     vcpu=document.getElementById('dvCpu')
+     vbola=document.getElementById('dvBola')
+     vPaineltxtPontos=document.getElementById('txtPontos')
+     document.addEventListener('keydown',teclaDw)
+     document.addEventListener('keyup',teclaUp)
+ }
+
+ window.addEventListener("load",inicializa)
